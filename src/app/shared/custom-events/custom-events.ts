288015a5385f75d05b19d25034807ac933bac980
Your Name <n180606@rguktn.ac.in>
2024-08-31 22:39:13 +0530

    feat: kink-pointerDown

--- src/app/shared/custom-events/custom-events.ts
+++ src/app/shared/custom-events/custom-events.ts
@@ -10,6 +10,7 @@ import { INormalizedLandmark } from "../../utilities/vg-types-handlandmarks";
 import { VgHandLandmarksDTO } from "../DTO/vg-handlandmark";
 import { EFingers } from "../../utilities/vg-constants";
 import { threadId } from "worker_threads";
+import { timeStamp } from "console";
 
 export class VgPointer
   extends AVgPointerEvents
@@ -25,10 +26,12 @@ export class VgPointer
   mouseInit!: MouseEventInit;
   props!: IGestureCustomProps;
   structuredLandmarks?:VgHandLandmarksDTO;
-  freezeFlag?: INormalizedLandmark;
+  palmHeight?: number;
+  fingerHeight?: number;
+  fingerKinkRatio?: number;
 
+  kinkWindow: [number, number]= [null!, null!];
   motionWindow: [INormalizedLandmark, INormalizedLandmark, INormalizedLandmark] = [null!, null!, null!]; // INDEX_MCP, INDEX_MCP, INDEX_TIP
-  upWindow: [INormalizedLandmark, INormalizedLandmark] = [null!, null!];
   downWindow: [INormalizedLandmark, INormalizedLandmark] = [null!, null!];
 
 
@@ -58,6 +61,7 @@ export class VgPointer
 
 
 
+    // console.log( "__________", this.structuredLandmarks?.state['INDEX']);
 
     // // Applied Pressure: Finger is closed
     // if (
@@ -164,10 +168,10 @@ export class VgPointer
   trigger() {
     this.staticEventsInitialiser();
     this.isPointerDown();
-    this.isPointerUp();
-    this.isPointerClick();
-    this.isPointerDrag();
-    this.isPointerDrop();
+    // this.isPointerUp();
+    // this.isPointerClick();
+    // this.isPointerDrag();
+    // this.isPointerDrop();
     // if(this.isPointerDown() || this.isPointerUp()){
 
     // }else 
@@ -191,18 +195,27 @@ export class VgPointer
   }
 
   private isPointerDown():boolean{
+
+    // console.log("KKKKKKKKKK",this.kinkWindow);
     // Applied Pressure: Finger is closed
     if (
       this.structuredLandmarks &&
       this.downWindow[0] != null &&
       this.downWindow[1] == null &&
-      this.structuredLandmarks.state["INDEX"] < 0.85
+      // ____STATIC ALGORITHM__________  this.structuredLandmarks.state["INDEX"] < 0.85
+      this.kinkWindow[0]!= null && 
+      this.kinkWindow[1]== null &&
+      this.kinkWindow[0]- this.fingerKinkRatio >= 100
     ) {
+
+      
+      this.kinkWindow[1]= this.fingerKinkRatio; 
+
       this.downWindow[1] = this.structuredLandmarks.data['INDEX'].TIP;
       this.motionWindow[0]= this.structuredLandmarks.data['INDEX'].MCP;
       this.motionWindow[1]= this.structuredLandmarks.data['INDEX'].MCP;
       console.log(
-        "**************ON POINTER DOWN EVENT TRIGGERED!!!!!**************",this.downWindow[0]      );
+        "**************ON POINTER DOWN EVENT TRIGGERED!!!!!**************",this.kinkWindow     );
         return true;
 
     }
@@ -216,7 +229,10 @@ export class VgPointer
       this.downWindow[0] != null &&
       this.downWindow[1] != null &&
       this.motionWindow[0] != null &&
-      this.structuredLandmarks.state["INDEX"] > 0.85
+      // ____STATIC ALGORITHM__________  this.structuredLandmarks.state["INDEX"] > 0.85
+      this.kinkWindow[0]!= null &&
+      this.kinkWindow[1]!= null &&
+      (this.fingerKinkRatio! - this.kinkWindow[1]) >= 100
     ) {
       console.log(
         "*************ON POINTER UP TRIGGERED!!!!!********************",
@@ -228,18 +244,24 @@ export class VgPointer
     return false;
   }
 
-  private isPointerClick():boolean{
-    if (this.structuredLandmarks && this.downWindow[0]!= null && this.downWindow[1]!=null && this.motionWindow[0] !=null && this.structuredLandmarks.state["INDEX"]>0.85 &&
-      weightedEuclideanDistance( this.motionWindow[0], this.structuredLandmarks.data['INDEX'].MCP, [1,1]) < 0.08
-     ){
-        console.log("___________Click**************");
-        this.downWindow[1]= null!;
-        this.motionWindow[1]= null!;
-        return true;
-    }
-    return false;
+  // private isPointerClick():boolean{
+  //   if (this.structuredLandmarks && this.downWindow[0]!= null && this.downWindow[1]!=null && this.motionWindow[0] !=null && 
+  //    // STACTIC: this.structuredLandmarks.state["INDEX"]>0.85 &&
+  //    this.kinkWindow[0]!= null &&
+  //    this.kinkWindow[1]!= null &&
+  //    this.fingerKinkRatio - this.kinkWindow[1] >= 100 &&
+  //     weightedEuclideanDistance( this.motionWindow[0], this.structuredLandmarks.data['INDEX'].MCP, [1,1]) < 0.08
+  //    ){
+  //       console.log("___________Click**************");
+  //       this.kinkWindow[0]= this.fingerKinkRatio;
+  //       this.kinkWindow[1]= null;
+  //       this.downWindow[1]= null!;
+  //       this.motionWindow[1]= null!;
+  //       return true;
+  //   }
+  //   return false;
 
-  }
+  // }
 
   private isPointerDrop():boolean{
     if( this.structuredLandmarks && this.downWindow[0]!= null && this.downWindow[1]!= null && this.motionWindow[0]!= null && this.structuredLandmarks.state["INDEX"]> 0.85 &&
@@ -278,6 +300,7 @@ export class VgPointer
   }
 
   private staticEventsInitialiser(){
+
     // Structuring raw landmarks
     const landmark: INormalizedLandmark[] = this.props.currentLandmarks;
     this.structuredLandmarks = new VgHandLandmarksDTO(landmark);
@@ -319,40 +342,50 @@ export class VgPointer
 
       this.structuredLandmarks.state[finger] = Math.pow(
         MCPtoTIPDistance / piecewiseDistance,
-        2,
+        1,     // Change to power(_,2) to get the probability
       );
     }
-
-    // weightedEuclideanDistance( this.props.previousLandmarks[5], this.structuredLandmarks.data["INDEX"].MCP, [1, 1],) < 0.08
     
-    // Dynamic run-time Initialization
-    // this.MCPWindow[0] = this.structuredLandmarks.data["INDEX"].MCP;
-    // console.log( "___________________", this.props.previousLandmarks[5]);
+    
+
+    this.palmHeight= weightedEuclideanDistance( this.structuredLandmarks.data['WRIST'].WRIST, this.structuredLandmarks.data['INDEX'].MCP, [1,1]);
+    this.fingerHeight= weightedEuclideanDistance( this.structuredLandmarks.data["INDEX"].TIP, this.structuredLandmarks.data["INDEX"].MCP, [1,1]);
+    
+    this.fingerKinkRatio= 1000* this.fingerHeight/ this.palmHeight;
+    console.log("RRRRRRRRRRRRRRRRRRr", this.fingerKinkRatio);
+    
+  
+    
+    
 
 
-    // No Pressure: Finger is erected
-    if ( // isNaN(this.downWindow[0]) &&
-      // this.downWindow[0]== null &&
-      // this.downWindow[1]== null &&
-      this.structuredLandmarks.state["INDEX"] > 0.985
-    ) {
-        // Freeze the pointer at INDEX_TIP
-        if( this.downWindow[0] == null){
-          this.freezeFlag= this.structuredLandmarks.data['INDEX'].TIP;
-          this.downWindow[0] = this.structuredLandmarks.data['INDEX'].TIP;
-        }
+    // console.log( "___________________filter",this.mouseInit.clientY,this.structuredLandmarks.state['INDEX'],this.structuredLandmarks.state['INDEX']*document.body.clientHeight);
 
-        if(this.motionWindow[0]== null){
-          this.motionWindow[0]= this.structuredLandmarks.data['INDEX'].MCP;
-        }
 
-        // if( weightedEuclideanDistance(this.motionWindow[0], this.structuredLandmarks.data['INDEX'].MCP, [1,1])> 0.08 ){
+    // ______________________________________________
+    //           FLUID ALGORITHM
+    //_______________________________________________
+    // Very first instance of detection
+    if(this.kinkWindow[0]== null && this.kinkWindow[1]== null){
+        this.kinkWindow[0]= this.fingerKinkRatio;
+        this.downWindow[0] = this.structuredLandmarks.data['INDEX'].TIP;
+        this.motionWindow[0]= this.structuredLandmarks.data['INDEX'].MCP;
+    }
 
-        //   this.downWindow[0] = this.structuredLandmarks.data['INDEX'].TIP;
-        //   this.motionWindow[0]= this.structuredLandmarks.data['INDEX'].MCP;
-        // }
-        
-       
-      }
+    // ______________________________________________
+    //           STATIC ALGORITHM
+    //_______________________________________________
+    // // No Pressure: Finger is erected
+    // if ( this.structuredLandmarks.state["INDEX"] > 0.985) {
+    //     // Freeze the pointer at INDEX_TIP
+    //     if( this.downWindow[0] == null){
+    //       this.downWindow[0] = this.structuredLandmarks.data['INDEX'].TIP;
+    //     }
+
+    //     if(this.motionWindow[0]== null){
+    //       this.motionWindow[0]= this.structuredLandmarks.data['INDEX'].MCP;
+    //     }
+    //   }
+    
   }
 }
